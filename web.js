103a72cff92f50bbcadb674d62f0ec7c6234f63f
olexiy <olexiy@olexiy>
2013-07-11 23:10:18 +0300

    reading from file

--- web.js
+++ web.js
@@ -1,12 +1,20 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    fs.readFile('index.html', 'utf8', function(err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(data);
+        var buf = new Buffer(data);
+        response.send(buf.toString());
+    });
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
